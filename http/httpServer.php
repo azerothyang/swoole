1269600cf23ad745bc65992de52f8b12be23e109
azerothYang <azerothyangg@gmail.com>
2018-03-27 18:02:18 +0800

    ws && http

---
+++ http/httpServer.php
@@ -0,0 +1,8 @@
+<?php
+$server = new swoole_http_server("0.0.0.0", 9501);
+$server->on("request", function (swoole_http_request $request, swoole_http_response $response){
+    $response->header('Server', 'qhdata_server');
+//    $response->write("hello http");
+    $response->end("hello world");
+});
+$server->start();
